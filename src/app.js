03462a2fb62865abfaff61b1ac0416efcc30e79e
Diogoribeiro11 <diogoamaroribeiro11@gmail.com>
2022-10-14 14:25:16 +0000

    Excuse Generator

--- src/app.js
+++ src/app.js
@@ -4,8 +4,34 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { Button } from "bootstrap";
 
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+function excuse() {
+  return (
+    who[Math.floor(Math.random() * who.length)] +
+    " " +
+    action[Math.floor(Math.random() * action.length)] +
+    " " +
+    what[Math.floor(Math.random() * what.length)] +
+    " " +
+    when[Math.floor(Math.random() * when.length)]
+  );
+}
+
+var btn = document.getElementById("button");
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  button.onclick = function() {
+    document.getElementById("excuse").innerHTML = excuse();
+  };
 };
